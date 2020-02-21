c29fdf099d7c912efd3df0de5dca303ef3ee04e6
galanca <cesar.galan@colorado.edu>
2020-02-21 14:29:46 -0700

    work on Searching for a specifc UUID

--- Test/App/BluetoothTest/app/src/main/java/com/example/bluetoothtest/MainActivity.java
+++ Test/App/BluetoothTest/app/src/main/java/com/example/bluetoothtest/MainActivity.java
@@ -1,36 +1,65 @@
 package com.example.bluetoothtest;
 
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ParcelUuid;
 import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
+    //private LeDeviceListAdapter leDeviceListAdapter;
+    private Handler handler;
+    private boolean mScanning;
+    //private LeDeviceListAdapter leDeviceListAdapter;
     private static final int REQUEST_ENABLE_BT = 0;
     private static final int REQUEST_DISCOVER_BT = 1;
+    private static final long SCAN_PERIOD = 10000;
+    public final static UUID UUID_Service = UUID.fromString("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+    public final static UUID UUID_CHARACTERISTCS = UUID.fromString("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+
 
     TextView mStatusBlueTv, mPairedTv;
     ImageView mBlueIv;
     Button mOnBtn, mOffBtn, mDiscoverBtn, mPairedBtn;
 
     BluetoothAdapter mBlueAdapter;
+    BluetoothLeScanner mBluetoothscanner = mBlueAdapter.getBluetoothLeScanner();
+    ScanFilter.Builder builder = new ScanFilter.Builder();
+
+    private ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            mPairedTv.setText("Found Device");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //SERVICE_UUID    = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-        //CHARACTERISTICS_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
         mStatusBlueTv   = findViewById(R.id.statusBluetoothTv);
         mPairedTv       = findViewById(R.id.pairedTv);
         mBlueIv         = findViewById(R.id.bluetoothIv);
@@ -74,14 +103,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         //discover bluetooth btn click
         mDiscoverBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!mBlueAdapter.isDiscovering()) {
                     showToast("Making Your Device Discoverable");
-                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                    startActivityForResult(intent, REQUEST_DISCOVER_BT);
+                    //Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    //startActivityForResult(intent, REQUEST_DISCOVER_BT);
+
+                    //mBlueAdapter.startLeScan(new UUID[]{UUID_Service}, leScanCallback);
+
+                    // Filter BLE Device
+                    builder.setServiceUuid(ParcelUuid.fromString("4fafc201-1fb5-459e-8fcc-c5c9c331914b"));
+                    Vector<ScanFilter> filter = new Vector<ScanFilter>();
+                    filter.add(builder.build());
+
+                    // Scan Settings
+                    ScanSettings.Builder builderScanSettings = new ScanSettings.Builder();
+                    builderScanSettings.setScanMode(ScanSettings.SCAN_MODE_BALANCED);
+                    builderScanSettings.setReportDelay(0);
+
+
+                    mBluetoothscanner.startScan(filter, builderScanSettings.build(), mScanCallback);
+                    mPairedTv.setText("Scanning for ESP32");
+
+
+                    /*
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mScanning = false;
+                            mBlueAdapter.stopLeScan(leScanCallback);
+                        }
+                    }, SCAN_PERIOD);
+
+                    mScanning = true;
+                    mBlueAdapter.startLeScan(UUID_CHARACTERISTCS, leScanCallback);*/
                 }
             }
         });
@@ -144,4 +203,74 @@ public class MainActivity extends AppCompatActivity {
     private void showToast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
+
+
+
+    /*
+    // Le Device List adapter Class
+    private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+        }
+    };
+
+    // Adapter for holding devices found through scanning.
+    private class LeDeviceListAdapter extends BaseAdapter {
+        private ArrayList<BluetoothDevice> mLeDevices;
+        private LayoutInflater mInflator;
+        public LeDeviceListAdapter() {
+            super();
+            mLeDevices = new ArrayList<BluetoothDevice>();
+            mInflator = MainActivity.this.getLayoutInflater();
+        }
+        public void addDevice(BluetoothDevice device) {
+            if(!mLeDevices.contains(device)) {
+                mLeDevices.add(device);
+            }
+        }
+        public BluetoothDevice getDevice(int position) {
+            return mLeDevices.get(position);
+        }
+        public void clear() {
+            mLeDevices.clear();
+        }
+        @Override
+        public int getCount() {
+            return mLeDevices.size();
+        }
+        @Override
+        public Object getItem(int i) {
+            return mLeDevices.get(i);
+        }
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ViewHolder viewHolder;
+            // General ListView optimization code.
+            if (view == null) {
+                view = mInflator.inflate(R.layout.listitem_device, null);
+                viewHolder = new ViewHolder();
+                viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
+                viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
+                view.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+            BluetoothDevice device = mLeDevices.get(i);
+            final String deviceName = device.getName();
+            if (deviceName != null && deviceName.length() > 0)
+                viewHolder.deviceName.setText(deviceName);
+            else
+                viewHolder.deviceName.setText(R.string.unknown_device);
+            viewHolder.deviceAddress.setText(device.getAddress());
+            return view;
+        }
+    }
+    static class ViewHolder {
+        TextView deviceName;
+        TextView deviceAddress;
+    }*/
 }
