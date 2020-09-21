d1add9cc8bac33493ce32341dde4e91de462feca
galanCA <cega5137@colorado.edu>
2020-09-21 13:43:14 -0600

    proof of concept website working

---
+++ App/Python/HTML/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, jsonify, Response
+import random
+from datetime import datetime
+import json
+import time
+
+app = Flask(__name__)
+random.seed()
+
+
+@app.route('/', methods=['GET'])
+def index():
+	message = random.randint(1,100)
+
+
+	return render_template('index.html', message=message)
+
+@app.route('/findESP32')
+def findESP32():
+	print("Looking....")
+	return ("nothing")
+
+@app.route('/chart-data', methods=['GET'])
+def chart_data():
+	def generate_random_data():
+		while True:
+			json_data = json.dumps(
+				{'time':datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value':random.random()*100 })
+			yield f"data:{json_data}\n\n"
+			time.sleep(0.5)
+
+	return Response(generate_random_data(), mimetype='text/event-stream')	
+
+
+if __name__ == '__main__':
+	app.run(debug=True, host='192.168.0.114',port=6600)
